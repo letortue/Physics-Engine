7909d53debe15d42c6d18b474ed68dccc44f58cc
Marek <marek@ezoz.com.pl>
2026-04-13 20:28:30 +0200

    Added config class and .json

---
+++ Physics Engine/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+namespace Physics_Engine
+{
+    internal class Config
+    {
+        public int[] window_res {get; set;}
+        public int interval { get; set; }
+        
+    }
+}
